cec9da5947b493bf6d66cfbc38e1059480ba909c
Noaseto <noix.50@hotmail.fr>
2026-08-04 08:25:20 +0200

    pushing temp work, so i can still update this in holydays
    it seems the api service might need an update for what i want it to do (display a transparant window like a toaster, anywhere, without taking control above input, resizable, movable)

--- src/mod.cpp
+++ src/mod.cpp
@@ -22,8 +22,27 @@ static void handle_chat_event(const TwitchEvent& event) {
     snprintf(buf, sizeof(buf), "%s", event.data.c_str());
     svc_log->info(mod_ctx, buf);
 }
+//
+// UiDialogDesc ui_dialog_desc = UI_DIALOG_DESC_INIT;
+// UiDialogHandle ui_dialog_handle = UI_SCOPE_OVERLAY;
+// UiWindowDesc ui_window_desc = UI_WINDOW_DESC_INIT;
+// UiWindowHandle ui_window_handle;
 
 MOD_EXPORT ModResult mod_initialize(ModError*) {
+    // ui_dialog_desc.title="Twitch chat";
+    // ui_dialog_desc.body_rml="ouais ya du contenue wouhou<span> pouet \n ah <\span> wow<\br>le sauit de lighnet";
+    // ui_dialog_desc.action_count=1;
+    // UiDialogAction ui_dialog_action = {"un label", [](ModContext*, UiDialogHandle, void*){}, nullptr, true};
+    //
+    // ui_dialog_desc.actions={&ui_dialog_action};
+    // ui_dialog_desc.on_dismiss=[](ModContext*, UiDialogHandle, void*){};
+    // ui_dialog_desc.user_data=nullptr;
+    // svc_ui->dialog_push(mod_ctx, &ui_dialog_desc, &ui_dialog_handle);
+
+    UiTabDesc tabs[1]={UI_TAB_DESC_INIT};
+    // ui_window_desc.tabs = tabs;
+    // ui_window_desc.tab_count=1;
+
     svc_log->info(mod_ctx, "TwitchChat mod initialized");
     return MOD_OK;
 }
@@ -43,6 +62,8 @@ MOD_EXPORT ModResult mod_update(ModError*) {
     // Iterate over all the events we see
     for (uint32_t eventIndex = 0; eventIndex < eventCount; ++eventIndex) {
         handle_chat_event(events[eventIndex]);
+        // ui_dialog_desc.body_rml=events[eventIndex].data.c_str();
+
     }
 
     return MOD_OK;
